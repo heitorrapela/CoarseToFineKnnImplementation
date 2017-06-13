827b2acd5e527e77b434a6ece610e2fcb0922ead
Gabriel Bandeira <gmb@cin.ufpe.br>
2017-06-13 10:54:50 -0300

    Add script to install the required libraries

---
+++ installRequiredLibs.sh
@@ -0,0 +1,3 @@
+sudo pip install pandas
+sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose -y
+sudo pip install scikit-learn
