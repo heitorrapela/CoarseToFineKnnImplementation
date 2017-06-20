cc43a5506b8bee5bf0bcb3aea9da9ecace2f97b6
Heitor Rapela <hrm@cin.ufpe.br>
2017-06-20 12:46:31 -0300

    Add implementations with Iris Dataset

---
+++ centerBasedNN_iris.py
@@ -0,0 +1,121 @@
+# Center Based NN - Iris Dataset - Machine Learning Project - CIn/UFPE
+# Members:
+# Gabriel de Franca Medeiros
+# Gabriel Marques Bandeira
+# Heitor Rapela Medeiros 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import timeit
+import time as time
+#import plotly.plotly as py
+#from plotly.graph_objs import *
+#import plotly.tools as tls
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from collections import Counter
+from numpy import linalg as LA
+
+# Separate data from Iris dataset
+def separateData(data):
+	# y = Class 
+	y = data.ix[:,0].values
+	# X = Data split in att ; Matrix : qtdElement x att
+	X = data.ix[:,1:att+1].values
+	X_std = StandardScaler().fit_transform(X)
+
+	return y,X,X_std
+
+# mi : Element of Real Numbers
+def calculate_mi(x1,x2,x):
+	aux1 = np.dot(x-x1,x2-x1)
+	aux2 = np.dot(x2-x1,x2-x1) 
+	if(aux2 != 0):
+		mi = aux1/float(aux2)
+	else:
+		mi = 0.0
+	return mi
+
+# p : projection point of x in x1x2
+def calculate_p(x1,x2,mi):
+	p = x1 + mi*(x2-x1)
+	return p
+
+def calculate_distance_x1x2(x,p):
+	return LA.norm(x-p)
+
+def distance_nearestFeatureLine(x1,x2,x):
+	mi = calculate_mi(x1,x2,x)
+	p = calculate_p(x1,x2,mi)
+	dist = calculate_distance_x1x2(x,p)
+	return dist
+
+################## Data pre-processing ###########################
+# Number of att = 13
+att = 4
+# Class quantity
+numCat = 3  
+
+# Data Base: Iris 
+dataTraining = pd.read_csv('iris/iris_train.txt', sep=",", header = None)
+# To help manipulate, hashtable
+dataTraining.columns = ["Class",0,1,2,3]
+
+# Data Base: Iris 	
+dataTest = pd.read_csv('iris/iris_test.txt', sep=",", header = None)
+# To help manipulate, hashtable
+dataTest.columns = ["Class",0,1,2,3]
+
+
+#finding classes centers
+classesCenter = []
+for n_class in range(0, numCat):
+	aux = dataTraining[dataTraining['Class'] == n_class].mean()
+	classesCenter.append(np.asarray(aux[1:len(aux)]))
+
+# Separate Test Data #
+class_label_test, Y_semStd, Y = separateData(dataTest)
+
+K = range(1,11)
+K_rate = [0,0,0,0,0,0,0,0,0,0]
+
+print class_label_test
+
+# Get each instance of the test from db
+start = time.time()
+
+counter = 0
+for x in Y:
+	# Y shape : (att,) change to (1,att) 
+	x = np.array([x])
+	dist = []
+	for c in range(0,numCat):
+		center = classesCenter[c]
+		# print center
+		aux = dataTraining[dataTraining['Class'] == c]
+		class_label_tranning, aux_semStd, Aux = separateData(aux)
+		for i in range(len(Aux)):
+			x1 = Aux[i]
+			dist_aux = distance_nearestFeatureLine(x1, center,x)
+			dist.append([c, dist_aux])
+	dist_ = sorted(dist, key=lambda x: x[1])
+
+
+	for k in range(len(K)):
+		#get the k first elements in the list
+		aux_ar = dist_[:k+1]
+		#get the classes
+		classes = [x[0] for x in aux_ar]
+		classes = np.array(classes)
+		#get the most common class in the array
+		counts = np.bincount(classes)
+		result = np.argmax(counts)
+
+		#compare with the known class
+		if result == class_label_test[counter]:
+			K_rate[k] = K_rate[k] + 1
+
+	counter = counter + 1
+
+#calculating the rate 
+print [x/float(len(Y)) for x in K_rate]
---
+++ coarseToFineKnn_iris.py
@@ -0,0 +1,165 @@
+# Coarse to fine KNN Classifier - Machine Learning Project - CIn/UFPE
+#
+# Members:
+# Gabriel de Franca Medeiros (gfm@cin.ufpe.br)
+# Gabriel Marques Bandeira (gmb@cin.ufpe.br)
+# Heitor Rapela Medeiros (hrm@cin.ufpe.br)
+#
+# Based on Yong Xu algorithm proposed in http://dx.doi.org/10.1016/j.patrec.2013.01.028 (last acessed on 19/june/2017)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import timeit
+# import plotly.plotly as py
+import plotly as py
+from plotly.graph_objs import *
+import plotly.tools as tls
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from collections import Counter
+
+
+# Separate data from Iris dataset
+def separateData(data):
+	# y = Class 
+	y = data.ix[:,0].values
+	# X = Data split in att ; Matrix : qtdElement x att
+	X = data.ix[:,1:att+1].values
+	X_std = StandardScaler().fit_transform(X)
+
+	return y,X,X_std
+
+
+# Calculate gamma = power((XT*X + mi*I),-1)*XT*Y
+def calculate_gamma(X, Y):
+	# Calculate power((XT*X + mi*I),-1)*XT
+	invsXT = calculate_invs_xt(X)
+	# Calculate gamma = power((XT*X + mi*I),-1)*XT*Y
+	gamma = np.dot(invsXT,Y)
+
+	return gamma
+
+
+# Calculate power((XT*X + mi*I),-1)*XT
+def calculate_invs_xt(X):
+	# Calculate XT 
+	XT = X.T
+	# Calculate XT*X
+	XTX = np.dot(XT,X)
+	# Calculate I, same dimension of XT*X lines
+	I = np.identity(XTX.shape[0])
+	# Calculate mi*I
+	miI = mi*I
+	# Calculate inverse of (XT*X + mi*I)
+	invs = np.linalg.inv(XTX + miI)
+	# Calculate power((XT*X + mi*I),-1)*XT
+	invsXT = np.dot(invs,XT)
+
+	return invsXT
+
+
+################## Data pre-processing ###########################
+# Number of att = 4
+att = 4
+# Class quantity
+numCat = 3  
+
+# Data Base: Iris Dataset
+dataTraining = pd.read_csv('iris/iris_train.txt', sep=",", header = None)
+# To help manipulate, hashtable
+dataTraining.columns = ["Class",0,1,2,3]
+
+# Data Base: Iris Dataset
+dataTest = pd.read_csv('iris/iris_test.txt', sep=",", header = None)
+# To help manipulate, hashtable
+dataTest.columns = ["Class",0,1,2,3]
+
+# Separate Train Data #
+class_label_tranning, X_semStd, X = separateData(dataTraining)
+
+# Separate Test Data #
+class_label_test, Y_semStd, Y = separateData(dataTest)
+
+###################################################################
+# The notation used in the article is transpose of our database
+# so att : its lines and the instace : its columns, so we need to transpose
+# our data
+# Convert our database representation to article representation (Transpose input data) 
+# Matrix our representation: (instance,att)
+# Article representation: (att, instance) 
+X = X.T
+
+################## CFKNNC implementation #########################
+# N value of CFKNNC : K <= N
+N = [10,20,30,40,50,60,70]
+
+# K value of CFKNNC : K <= N
+K = [1,2,3,4,5]
+
+# mi : constant value in CFKNNC
+mi = 0.01
+
+best_option = [0, N[0], K[0]]
+
+# Calculate power((XT*X + mi*I),-1)*XT
+invsXT = calculate_invs_xt(X)
+taxa = []
+
+for n in N:
+	for k in K:
+		# Iterate test set
+		y_class = []
+		# Get each instance of the test from db
+		for y in Y:
+			# Y shape : (att,) change to (1,att) 
+			y = np.array([y])
+			# Convert Y shape to (att,1), article representation of data
+			y = y.T
+
+			# Calculate gamma = power((XT*X + mi*I),-1)*XT*Y
+			gamma = np.dot(invsXT,y)
+
+			error_y = []
+			for i in range(len(gamma)):
+				error_y.append(np.linalg.norm(y[:,0]-gamma[i][0]*X[:,i].T))
+			
+			indexes = np.array(range(len(error_y)))
+			t = np.c_[indexes, error_y]
+			error_y_ord = np.array(sorted(t, key=lambda a_entry: a_entry[1]))
+			Z = X[:, error_y_ord[0:n, 0].astype(int)]
+			class_z = class_label_tranning[error_y_ord[0:n, 0].astype(int)]
+
+			new_gamma = calculate_gamma(Z, y)
+			error_y = []
+			for i in range(len(new_gamma)):
+				error_y.append(np.linalg.norm(y[:,0]-new_gamma[i][0]*Z[:,i].T))
+			indexes = np.array(range(len(error_y)))
+			t = np.c_[indexes, error_y]
+			error_y_ord = np.array(sorted(t, key=lambda a_entry: a_entry[1]))
+			classes = class_z[error_y_ord[0:k, 0].astype(int)]
+
+			data = Counter(classes)
+			y_class.append(data.most_common(1)[0][0])
+
+		ok = 0
+		for i in range(len(class_label_test)):
+			# print "Y label: " + str(class_label_test[i]) + "; identified as " + str(y_class[i])
+			if class_label_test[i] == y_class[i]:
+				ok = ok + 1
+
+		taxa.append(float(ok)/len(class_label_test)*100)
+		if taxa[-1] > best_option[0]:
+			best_option[0] = taxa[-1]
+			best_option[1] = n
+			best_option[2] = k
+		print "N = " +str(n) + "; K = " + str(k) + "\tTaxa: " + str(taxa[-1]) + "%"
+
+print 'Best option: ' + str(best_option[0]) + '%   -   N = ' + str(best_option[1]) + '; K = ' + str(best_option[2])
+x = N
+y = taxa
+title = 'CFKNNC - K: ' + str(K[0]) + ''
+py.offline.plot({
+		"data": [Scatter(x=x, y=y)],
+		"layout": Layout(title=title)
+	})
